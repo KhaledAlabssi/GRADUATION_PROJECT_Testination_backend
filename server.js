e4a29090c39e481489a0376ef9cfe8c901a30c89
KhaledAlabssi <khaled.absi5@gmail.com>
2021-09-21 14:57:53 +0200

    structring app routes, adding dotenv

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
+const teacherRouter = require('./routes/teacher')
+app.use('/teacher', teacherRouter);
 app.get('/', (req, res) => {
     res.send("Hi")
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Server is running on 3000 or process.env.PORT ")
+})
