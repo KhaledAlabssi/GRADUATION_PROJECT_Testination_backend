fba9ebad7f04d915a6bda5d3e682596dbb4358f6
KhaledAlabssi <khaled.absi5@gmail.com>
2021-09-24 14:31:00 +0200

    create studnets fun, routes, controllers

--- controllers/students.js
+++ controllers/students.js
@@ -15,6 +15,39 @@ function allstudents(req, res) {
     })
 }
 
+function getStudent(req, res) {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connection as id....... ${connection.threadId}`)
+        connection.query('select * from users where name =?',`${req.params.name}`, (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.json(rows)
+            } else {
+                console.log(err)
+            }
+        })
+    })
+}
+
+function createStudent(req, res) {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log(`connection as id....... ${connection.threadId}`)
+        console.log(req);
+        connection.query("INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, 'khaled', 't@t', '123')", (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.json(rows)
+            } else {
+                console.log(err)
+            }
+        })
+    })
+}
+
 module.exports = {
-    allstudents
+    allstudents,
+    getStudent,
+    createStudent
 }
--- routes/students.js
+++ routes/students.js
@@ -5,5 +5,9 @@ const getstudents = require('../controllers/students')
 
 router.get('/', getstudents.allstudents)
 
+router.post('/new', getstudents.createStudent)
+
+router.get('/:name', getstudents.getStudent)
+
 
 module.exports = router;
